7ce95244b16ee52382b39e58b81183e1991db965
Yomi Toba <ytwguru@gmail.com>
2019-09-20 00:55:46 -0500

    Update

--- pages/[page].js
+++ pages/[page].js
@@ -122,7 +122,7 @@ export default props => {
         result = <Search {...props} />;
         break;
       default:
-        result = getTemplates();
+        result = getTemplates(page);
     }
   }
 
