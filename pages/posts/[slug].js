95e78f78abe9981a1499120b7ab555eaed3b8f48
Yomi Toba <ytwguru@gmail.com>
2020-01-06 02:27:54 -0600

    Revert changes to video page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/react-hooks";
@@ -12,59 +12,54 @@ import { GET_PAGE_INFO, GET_PAGE_CATEGORY } from "../../lib/graphql";
 const Posts = props => {
   const { playingLive } = props;
   const [count, setCount] = useState(0);
-  const [relatedResults, setRelatedResults] = useState([]);
   const router = useRouter();
   const {
     query: { slug }
   } = router;
-  const variables = { slug };
 
+  const variables = { slug };
   const { data, fetchMore } = useQuery(GET_PAGE_INFO, {
-    variables
+    variables,
+    notifyOnNetworkStatusChange: true
   });
 
   const { post, podcasts } = data || {};
   const { categories, postDetails, title, excerpt, content, link } = post || {};
   const { relatedPosts } = postDetails || {};
   let result = { ...data };
-  let categoryList = [];
+  let currentPosts = [];
   if (postDetails) {
     if (categories && categories.edges) {
-      categoryList = categories.edges.map(obj => obj.node.categoryId);
+      const categoryList = categories.edges.map(obj => obj.node.categoryId);
+      if (!relatedPosts || relatedPosts.length < 3) {
+        fetchMore({
+          query: GET_PAGE_CATEGORY,
+          variables: { categories: categoryList },
+          updateQuery: (prev, { fetchMoreResult }) => {
+            result = prev;
+            if (prev && fetchMoreResult) {
+              if (!result.post.postDetails.relatedPosts)
+                result.post.postDetails.relatedPosts = [];
+
+              const { categoryPosts } = fetchMoreResult;
+              currentPosts = result.post.postDetails.relatedPosts;
+              if (categoryPosts && categoryPosts.edges) {
+                currentPosts = [...currentPosts, ...categoryPosts.edges];
+                result.post.postDetails.relatedPosts = currentPosts;
+              }
+            }
+            setCount(count + 1);
+            return result;
+          }
+        });
+      }
+
+      if (result.post.postDetails.relatedPosts)
+        result.post.postDetails.relatedPosts = result.post.postDetails.relatedPosts.filter(
+          n => (n.relatedPost ? n : null)
+        );
     }
   }
-
-  const loadMore = () =>
-    fetchMore &&
-    fetchMore({
-      query: GET_PAGE_CATEGORY,
-      variables: { categories: categoryList },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        result = { ...prev };
-        if (prev && fetchMoreResult) {
-          const { categoryPosts } = fetchMoreResult;
-          if (categoryPosts && categoryPosts.edges) {
-            setRelatedResults(
-              [...relatedResults, ...categoryPosts.edges].filter(n =>
-                n.relatedPost ? n : null
-              )
-            );
-          }
-        }
-        setCount(count + 1);
-        return result;
-      }
-    });
-
-  useEffect(() => {
-    if (relatedPosts && relatedPosts.length > 0) {
-      setRelatedResults(relatedPosts.filter(n => (n.relatedPost ? n : null)));
-    }
-    if (categoryList && relatedResults.length < 3)
-      if (categoryList.length > 0) {
-        loadMore();
-      }
-  }, [categoryList]);
 
   const description =
     excerpt || content || "On Demand Arts, Culture & Education Programming";
@@ -96,11 +91,14 @@ const Posts = props => {
               }}
             />
           )}
-          {result.post && relatedResults && relatedResults.length > 0 && (
+          {result.post && result.post.postDetails.relatedPosts && (
             <ListOfPosts
               title="Related Posts"
               posts={
-                relatedResults.map(obj => obj && obj.relatedPost).slice(0, 3) ||
+                (result.post.postDetails.relatedPosts &&
+                  result.post.postDetails.relatedPosts
+                    .map(obj => obj && obj.relatedPost)
+                    .slice(0, 3)) ||
                 []
               }
               link={{ page: "posts" }}
